a1d9dfa152630b56a4b63e1940944306cc3e6868
Vibush <vibushks@tamu.edu>
2022-02-27 00:17:57 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,10 @@ class MoviesController < ApplicationController
     end
   
     def index
-      @movies = Movie.all
+      @sort = params[:sort]
+      puts "Sort value:"
+      puts @sort
+      @movies = Movie.all.order(@sort)
     end
   
     def new
